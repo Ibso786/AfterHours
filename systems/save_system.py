a7a37389251c9bfd3dbe9f0f95736e0a75d62065
Ibso786 <Ibrahim10.rayaz@gmail.com>
2026-06-08 22:19:26 +0100

    Update save_system.py

--- systems/save_system.py
+++ systems/save_system.py
@@ -1,14 +1,23 @@
+import json
+import os
 
-import json, os
-
-DEFAULT={
-'cash':5000,
-'rep':0,
-'cars':['Civic EG']
+DEFAULT = {
+    "cash": 5000,
+    "rep": 0,
+    "fuel": 100,
+    "cars": ["Civic EG"],
+    "current_car": "Civic EG"
 }
 
-def load_save(path='saves/profile.json'):
+def load_save(path="saves/profile.json"):
     if not os.path.exists(path):
         return DEFAULT.copy()
-    with open(path) as f:
+
+    with open(path, "r") as f:
         return json.load(f)
+
+def save_game(data, path="saves/profile.json"):
+    os.makedirs("saves", exist_ok=True)
+
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
